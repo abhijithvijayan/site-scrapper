c8742952330836e1cfb884ea5bb43ac60f754983
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2022-10-16 14:17:23 +0530

    feat: implement caching response

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config({path: `${__dirname}/.env`});
 
-const {isNull, isNullOrUndefined} = require("@abhijithvijayan/ts-utils");
+const {isNull, isNullOrUndefined, isNumber: isPureNumber, isString, isEmpty} = require("@abhijithvijayan/ts-utils");
 const chromium = require("@sparticuz/chromium");
 const puppeteer = require("puppeteer-core");
 const hash = require('object-hash');
@@ -12,11 +12,20 @@ const server = express();
 const deta = Deta(process.env.DETA_PROJECT_KEY);
 const db = deta.Base(process.env.DETA_DB_NAME);
 
+const timezoneOffset = 0; // for UTC
+const cacheTTL = 5 * 60 * 1000; // 5 minutes
+
 function adjustForTimezone(date, offset = 0) {
     const timeOffsetInMS = offset * 60 * 60 * 1000;
     date.setTime(date.getTime() + timeOffsetInMS);
 
     return date;
+}
+
+function getTimeInSeconds(date = new Date()) {
+    const today = adjustForTimezone(date, timezoneOffset);
+
+    return today.getTime() / 1000;
 }
 
 // 'a,b,c' => ['a', 'b', 'c']
@@ -93,21 +102,80 @@ const cacheEntry = async (req, res, next) => {
 }
 
 const getBrowser = async (req, res, next) => {
-    req._browser = await initPuppeteer();
+    if (isNullOrUndefined(req._cached)) {
+        // if cache exist, no need to initialize browser
+        req._browser = await initPuppeteer();
+    }
 
     next();
 }
 
-server.get('/', [
+const computeHash = (req, res, next) => {
+    // get hash from query params
+    req._hash = getCacheKey(req.query);
+    console.debug({msg: "hash", hash: req._hash});
+
+    next();
+}
+
+function isNumber(value) {
+    // returns true if number or a number string
+    return isPureNumber(value) || (isString(value) && !isEmpty(value) && isPureNumber(+value));
+}
+
+const getFromCache = async (req, res, next) => {
+    // get entry from DB
+    const entry = await db.get(req._hash);
+    // compute the cache TTL from query
+    const offsetMilliSeconds = isNumber(req.query.cacheTTL) ? Number(req.query.cacheTTL) : cacheTTL;
+
+    if (!isNull(entry)) {
+        // if cache entry falls into the cache TTL window, return that item
+        const currentTimeInSeconds = getTimeInSeconds(new Date());
+        const cacheTimestampInSeconds = getTimeInSeconds(new Date(entry.timestamp));
+        const offsetInSeconds = offsetMilliSeconds / 1000;
+
+        if (
+            // the current time should be greater than the cached timestamp
+            currentTimeInSeconds >= cacheTimestampInSeconds &&
+            // but within the cache window
+            currentTimeInSeconds < cacheTimestampInSeconds + offsetInSeconds
+        ) {
+            console.debug({msg: "cache exist", ttl: offsetMilliSeconds});
+            req._cached = entry;
+        } else {
+            console.debug({msg: "cache expired", ttl: offsetMilliSeconds});
+        }
+    } else {
+        console.debug({msg: "not in cache"});
+    }
+
+    next();
+}
+
+server.use((req, res, next) => {
+    // disable cache control for /api routes
+    // this removes the 304 status for requests
+    if (/^\/api\//.test(req.originalUrl)) {
+        res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+    }
+
+    return next();
+});
+
+// status check route
+server.get('/ping', (req, res) => {
+    return res.json('pong');
+});
+
+server.get('/api/v1/html', [
+    computeHash,
+    getFromCache,
     getBrowser,
     async (req, res, next) => {
         const url = req.query.url;
 
-        // get hash from query params
-        const hash = getCacheKey(req.query);
-        console.debug({msg: "hash", hash});
-
-        if (!isNull(req._browser) && !isNull(url)) {
+        if (isNullOrUndefined(req._cached) && !isNull(req._browser) && !isNull(url)) {
             let page = await req._browser.newPage();
             console.debug({msg: "loading page"});
 
@@ -125,10 +193,10 @@ server.get('/', [
 
             // payload to store in DB
             req._entry = {
-                key: hash,
+                key: req._hash,
                 url,
                 html: bodyHTML,
-                timestamp: adjustForTimezone(new Date()) // in UTC
+                timestamp: adjustForTimezone(new Date(), timezoneOffset) // in UTC
             };
         }
 
